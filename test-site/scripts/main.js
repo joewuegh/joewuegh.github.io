3b650a06661303b52c6768d1081a053e1cf91dea
joewuegh <joe.wuenschel@fedex.com>
2016-07-14 13:18:53 -0400

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+var myHeading = document.querySelector('h2');
+myHeading.textContent = 'Hello!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/carribean-image.jpg') {
+      myImage.setAttribute ('src','images/starfish-image.jpg');
+    } else {
+      myImage.setAttribute ('src','images/carribean-image.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Modern Web is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Modern Web is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
